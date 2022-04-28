10e5c1a40727e44b91d5367872a3e5a57a9d6daf
rishiraj4721 <55681094+rishiraj4721@users.noreply.github.com>
2022-04-28 20:42:31 +0530

    filter function

---
+++ IR_application/frontend/filter.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+def filter(result_list,filter_rating,filter_website,filter_difficulty):
+    '''
+    result_list: list of dict
+    filter_rating: float [0,5.0]
+    filter_website: boolean[Coursera,Edx,Udemy,Udacity]
+    filter_difficulty: boolean[Beginner,Intermediate,Advanced]
+    '''
+    df = pd.DataFrame(result_list)
+    # print(df.shape)
+
+    df = df[df['Ratings'] > filter_rating]
+    # print(df.shape)
+
+    difficulty_map = {0:'Beginner', 1:'Intermediate', 2:'Advanced'}
+    difs = []
+    for i in list(difficulty_map.keys()):
+        if filter_difficulty[i]:
+            difs.append(difficulty_map[i])
+    df = df[df['Difficulty Level'].isin(difs)]
+    # print(df.shape)
+
+    website_map = {0:'Coursera', 1:'Edx', 2:'Udemy', 3:'Udacity'}
+    webs = []
+    for i in list(website_map.keys()):
+        if filter_website[i]:
+            webs.append(website_map[i])
+    df = df[df['Website'].isin(webs)]
+    # print(df.shape)
+
+    return df.to_dict('records')
